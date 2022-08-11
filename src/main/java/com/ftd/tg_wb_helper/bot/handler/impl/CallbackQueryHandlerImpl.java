5eb06e2db9f6061f5aa11e7adc877787abe7f308
s0mething <denguetta@gmail.com>
2022-08-12 02:44:45 +0400

    Отправка в канал сообщения с клавиатурой

--- src/main/java/com/ftd/tg_wb_helper/bot/handler/impl/CallbackQueryHandlerImpl.java
+++ src/main/java/com/ftd/tg_wb_helper/bot/handler/impl/CallbackQueryHandlerImpl.java
@@ -1,11 +1,13 @@
 package com.ftd.tg_wb_helper.bot.handler.impl;
 
 import com.ftd.tg_wb_helper.bot.handler.CallbackQueryHandler;
+import com.ftd.tg_wb_helper.bot.keyboard.impl.InlineKeyboardMarkupBuilder;
 import com.ftd.tg_wb_helper.model.entity.TelegramUser;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 
 @Component
 public class CallbackQueryHandlerImpl implements CallbackQueryHandler {
@@ -14,10 +16,17 @@ public class CallbackQueryHandlerImpl implements CallbackQueryHandler {
     public PartialBotApiMethod<?> processCallbackQuery(CallbackQuery callbackQuery, TelegramUser telegramUser) {
         //chat_id Главного канала(Получилось отправлять туда сообщения)
         String chatId = "-1001720487296";
+        String chatId1 = "@test1337123";
         if (callbackQuery.getData().equals("1")) {
-            SendMessage sendMessage = new SendMessage();
+            SendMessage sendMessage = InlineKeyboardMarkupBuilder.create(chatId1)
+                    .row()
+                    .button("Da","da")
+                    .button("Manda","net")
+                    .endRow()
+                    .buildAsSendMessage();
             sendMessage.setText("test text");
             sendMessage.setChatId("@test1337123");
+
             return sendMessage;
         }
         return null;
